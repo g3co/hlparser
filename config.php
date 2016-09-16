e457f31b96b4753b5bbb9b270fab24ce61a7abba
valera <kabisovvaleriy@gmail.com>
2016-09-16 14:52:53 +0300

    Правка

--- config.php
+++ config.php
@@ -9,28 +9,6 @@
 class config {
     const ADMIN_EMAIL = "kabisovvaleriy@gmail.com";
 
-    const SITE_URL = 'http://honeylovers.ru';
-    const SITE_NAME= 'HoneyLovers.ru';
-    const SITE_INFO_EMAIL= 'info@honeylovers.ru';
-    const SITE_NOREPLY_EMAIL= 'no-reply@honeylovers.ru';
-
-	const DB_HOST = 'localhost';
-	const DB_USERNAME = 'honeylovers';
-	const DB_PASSWD = '7mPJ6xMS3LW003Zv';
-	const DB_NAME = 'honeylovers';
-	const CONSOLE_DEBUG = true;
-
-	const SEX_OPT_STORE = 0;
-
-    /** SMTP*/
-    const SMTP_SERVER = "smtp.yandex.ru";
-    const SMTP_USER = 'zakaz@honeylovers.ru';
-    const SMTP_PASS = '64ax4N3MQH6U2LFd';
-    const SMTP_PROTOCOL = 'ssl';
-    const SMTP_PORT= '465';
-
-
-	#ocFilter constants
 
 	const RUSIZE_ATRIBUTE_ID = 11; #ид атрибута - "российсикй размер"
 }
